5b6300b1849ad707d590aee237bd4906c52648cf
BasmaSalim2005 <160003526+BasmaSalim2005@users.noreply.github.com>
2025-03-04 11:12:38 +0100

    Update main.jsx7

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
 // main.jsx
-import React from 'https://unpkg.com/react@18/umd/react.production.min.js'; // Load React from CDN
-import ReactDOM from 'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'; // Load ReactDOM from CDN
-import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App.jsx';
 import './index.css';
-import { Provider } from 'https://unpkg.com/react-redux@8/umd/react-redux.min.js'; // Load React-Redux from CDN
-import store from './store.js';
+import { Provider } from 'react-redux';
+import store from './src/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,3 +15,4 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
+
