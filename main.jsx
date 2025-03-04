2766f00d8c026f86b095946667505a43cc237560
BasmaSalim2005 <160003526+BasmaSalim2005@users.noreply.github.com>
2025-03-04 11:08:53 +0100

    Update main.jsx2

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// main.jsx
+import React from 'https://unpkg.com/react@18/umd/react.production.min.js'; // Load React from CDN
+import ReactDOM from 'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'; // Load ReactDOM from CDN
 import App from './App.jsx';
 import './index.css';
-import { Provider } from 'react-redux';
+import { Provider } from 'https://unpkg.com/react-redux@8/umd/react-redux.min.js'; // Load React-Redux from CDN
 import store from './store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,4 +15,3 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
-
