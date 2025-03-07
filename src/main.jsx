9080654eff3f02508fc0bb3b346ee51b8658717c
basma <bsalim@constructor.university>
2025-03-07 12:11:31 +0100

    fix3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';
+import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './src/store.js';
+import store from './store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
